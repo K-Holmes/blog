4833f97590b9ae771fe861dca0834c4851d575cc
K-Holmes <Kayla.Holmes@cdpipelinedevshop.com>
2022-07-19 17:12:34 +0000

    Removed fake blog posts, working on template

---
+++ blog-post/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Blog from './Components/Blog';
+import Template from './Components/TemplatePage'
+
+function App() {
+  return (
+    <div className="App">
+      <Blog></Blog>
+      <Template></Template>
+    </div>
+  );
+}
+
+export default App;
